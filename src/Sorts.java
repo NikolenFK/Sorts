f79d36945941d89e7810f9b72ff79344914bd88b
Danylo <nikolenfk@gmail.com>
2023-11-16 23:39:06 +0100

    BubbleSort InsertionSort ShellSort MergeSort SelectionSort QuickSort

--- src/Sorts.java
+++ src/Sorts.java
@@ -1,11 +1,38 @@
 import java.util.Random;
+import java.util.Scanner;
 
+/**
+ * The Sorts class contains various sorting algorithms and a main method to test their performance.
+ */
 public class Sorts {
 
-    private static final int ARRAY_SIZE = 15_000;
-    private static final int MAX_RANDOM_INT = 100_000;
+    /**
+     * The size of the array to be sorted.
+     */
+    private static int ARRAY_SIZE;
 
+    /**
+     * The maximum random integer value for array elements.
+     */
+    private static int MAX_RANDOM_INT;
+
+    /**
+     * The scanner.
+     */
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    /**
+     * The main method that generates an array, performs different sorting algorithms,
+     * and prints the execution time of each algorithm.
+     *
+     * @param args The command line arguments (not used in this program).
+     */
     public static void main(String[] args) {
+
+        System.out.print("Enter the size of the array: ");
+        ARRAY_SIZE = SCANNER.nextInt();
+        System.out.print("Enter an integer max: ");
+        MAX_RANDOM_INT = SCANNER.nextInt();
         int[] array = generateArray(ARRAY_SIZE, MAX_RANDOM_INT);
         long startTime, endTime;
 
@@ -46,10 +73,23 @@ public class Sorts {
         printResult("QuickSort", endTime - startTime);
     }
 
+    /**
+     * Prints the result of a sorting algorithm, including the algorithm name and execution time.
+     *
+     * @param sortName   The name of the sorting algorithm.
+     * @param timeMillis The execution time of the sorting algorithm in milliseconds.
+     */
     private static void printResult(String sortName, long timeMillis) {
         System.out.printf("%s: %d ms\n", sortName, timeMillis);
     }
 
+    /**
+     * Generates an array of random integers.
+     *
+     * @param size     The size of the array to generate.
+     * @param maxValue The maximum random integer value for array elements.
+     * @return The generated array.
+     */
     private static int[] generateArray(int size, int maxValue) {
         int[] array = new int[size];
         Random random = new Random();
@@ -60,7 +100,11 @@ public class Sorts {
         return array;
     }
 
-    // BUBBLE_SORT
+    /**
+     * Sorts an array using the Bubble Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     */
     private static void bubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
@@ -73,7 +117,11 @@ public class Sorts {
         }
     }
 
-    // INSERTION_SORT
+    /**
+     * Sorts an array using the Insertion Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     */
     private static void insertionSort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int current = array[i];
@@ -88,7 +136,11 @@ public class Sorts {
         }
     }
 
-    // SHELL_SORT
+    /**
+     * Sorts an array using the Shell Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     */
     private static void shellSort(int[] array) {
         int n = array.length;
 
@@ -103,12 +155,14 @@ public class Sorts {
                 array[j] = temp;
             }
         }
-
     }
 
-    // MERGE_SORT
+    /**
+     * Sorts an array using the Merge Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     */
     private static void mergeSort(int[] array) {
-
         int length = array.length;
         if (length <= 1) return;
 
@@ -131,11 +185,16 @@ public class Sorts {
         mergeSort(leftArray);
         mergeSort(rightArray);
         merge(leftArray, rightArray, array);
-
     }
 
+    /**
+     * Merges two subarrays during the merge step of the Merge Sort algorithm.
+     *
+     * @param leftArray  The left subarray.
+     * @param rightArray The right subarray.
+     * @param array      The array to merge into.
+     */
     private static void merge(int[] leftArray, int[] rightArray, int[] array) {
-
         int leftSize = array.length / 2;
         int rightSize = array.length - leftSize;
 
@@ -167,7 +226,11 @@ public class Sorts {
 
     }
 
-    // SELECTION_SORT
+    /**
+     * Sorts an array using the Selection Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     */
     private static void selectionSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             int min_index = i;
@@ -177,9 +240,16 @@ public class Sorts {
             array[min_index] = array[i];
             array[i] = temp;
         }
+
     }
 
-    // QUICK_SORT
+    /**
+     * Sorts an array using the Quick Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     * @param start The starting index of the subarray to be sorted.
+     * @param end   The ending index of the subarray to be sorted.
+     */
     private static void quickSort(int[] array, int start, int end) {
         if (end <= start) return;
 
@@ -189,6 +259,14 @@ public class Sorts {
         quickSort(array, pivot + 1, end);
     }
 
+    /**
+     * Partitions the array during the partition step of the Quick Sort algorithm.
+     *
+     * @param array The array to be partitioned.
+     * @param start The starting index of the subarray to be partitioned.
+     * @param end   The ending index of the subarray to be partitioned.
+     * @return The index of the pivot after partitioning.
+     */
     private static int partition(int[] array, int start, int end) {
         int pivot = array[end];
         int i = start;
